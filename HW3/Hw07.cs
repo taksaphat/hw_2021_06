d1553d1acce5e2d20ad1e1bddb9a4e6247f9166d
taksaphat <76907073+taksaphat@users.noreply.github.com>
2021-06-14 13:02:26 +0700

    Commit homework 7

---
+++ HW3/Hw07.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+namespace HW3
+{
+    public interface IHomework07
+    {
+    string RemoveAndSortTextByAlphabetical(string text);
+    }
+    class Sort : IHomework07
+    {
+        /*public string n;
+        public Number(string text){
+            n = text;
+        }*/
+         // remove dupes and sort alphabetically
+        public string RemoveAndSortTextByAlphabetical(string text)
+            {
+                var result = string.Join(" ", text.Split(' ').Distinct().OrderBy(result => result));
+                Console.WriteLine(result);
+                return text;
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort sort = new Sort();
+            sort.RemoveAndSortTextByAlphabetical("hello world and practice makes perfect and hello world again");
+        }
+    }
+}
